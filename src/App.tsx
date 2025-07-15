87658db0daa780717da13fe840825b55cbfb11a9
Alex Martin <alexander.martin@jbknowledge.com>
2025-07-15 10:44:12 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [mensaje, setMensaje] = useState('');
+
+  const handleClick = () => {
+    setMensaje('Hola Mundo');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <button onClick={handleClick}>Haz clic</button>
+      {mensaje && <p>{mensaje}</p>}
     </div>
   );
 }
